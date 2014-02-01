b2cca728e5d5e8464d22d688f3cd69fdbab1331b
hecto932 <hecto932@gmail.com>
2014-02-01 15:47:46 -0430

    Cambios clase 5
    Uso de localStorage y sessionStorage

--- js/formulario.js
+++ js/formulario.js
@@ -1,30 +1,70 @@
-var $form = $('#formulario'),
+var $form 	= $('#formulario'),
 	$titulo = $('#titulo'),
-	$url = $('#url'),
+	$url 	= $('#url'),
 	$button = $('#mostrar-form'),
-	$list = $('#contenido'),
-	$post = $('.item').first();
+	$list 	= $('#contenido'),
+	$post 	= $('.item').first(),
+	ss 		= sessionStorage,
+	ls 		= localStorage;
+//Si existe el devolvera false o true
+if(ls.getItem('autosave'))
+{
+	$titulo.val(ss.getItem('Titulo'));
+	$url.val(ss.getItem('Url'));
+}
+
+var id = setInterval(function(){
+		ss.setItem('Titulo', $titulo.val());
+		ss.setItem('Url', $url.val());
+	},
+	1000
+	);
 
 //EVENTOS
 function mostrarFormulario()
 {
+
 	$form.slideToggle();
-	return false;
+	$list.slideToggle();
 }
 
-function agregarPost()
+function agregarPost(e)
 {
+	e.preventDefault();
+
 	var url = 	$url.val(), titulo = $titulo.val(),
 				$clone = $post.clone();
 
 	$clone.find('.titulo_item a').text(titulo).attr('href',url);
+	
 	$clone.hide();
 
 	$list.prepend($clone);
+	mostrarFormulario();
+	$titulo.val('');
+	$url.val('');
 	$clone.fadeIn();
-	$form.slideToggle();
-	return false;
 }
+/*
+function grabarInformacion(e)
+{
+	e.preventDefault();
+
+	var url = $url.val(),
+		titulo = $titulo.val(),
+		ls 	= localStorage,
+		ss	= sessionStorage;
+
+	ls.setItem('Titulo',titulo);
+	ls.setItem('Url',url);
+
+	ss.setItem('Titulo',titulo);
+	ss.setItem('Url',url);
+
+	mostrarFormulario();
+	$titulo.val('');
+	$url.val('');
+}*/
 
 //EVENTOS
 $button.click(mostrarFormulario);
